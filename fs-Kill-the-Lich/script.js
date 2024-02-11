30ee7a212098d1f04aac4de725d6448c22fb2fe4
Felipe-Simario <felipe.simario@gmail.com>
2024-02-11 22:04:00 +0000

    Add more functions

--- fs-Kill-the-Lich/script.js
+++ fs-Kill-the-Lich/script.js
@@ -2,9 +2,9 @@
 
 //Infos Hero
 const heroName = document.getElementById("heroName");
-let gold = 250;
+let gold = 50;
 const goldText = document.getElementById("goldText");
-let health = 100;
+let health = 150;
 const healthText = document.getElementById("healthText");
 let xp = 0;
 const xpText = document.getElementById("xpText");
@@ -14,10 +14,49 @@ let currentWeapon = 0;
 const inventory = [weapons[0]];
 
 //Infos Monster
-const monsters = ["DarkSlime", "Skeleton", "Zombie", "DarkOgre", "DarkElf", "DarkHighElf", "Lich"];
+const monsters = [
+                    {
+                        "monster name": "DarkSlime",
+                        "monster lvl": 1,
+                        "monster health": 20
+                    },
+                    {
+                        "monster name": "Skeleton",
+                        "monster lvl":2 ,
+                        "monster health": 85 
+                    },
+                    {
+                        "monster name": "Zombie",
+                        "monster lvl": 4,
+                        "monster health": 200 
+                    },
+                    {
+                        "monster name": "DarkOgre",
+                        "monster lvl": 7,
+                        "monster health": 370 
+                    },
+                    {
+                        "monster name": "DarkElf",
+                        "monster lvl": 10,
+                        "monster health": 400 
+                    },
+                    {
+                        "monster name": "DarkHighElf",
+                        "monster lvl": 15,
+                        "monster health": 500 
+                    },
+                    {
+                        "monster name": "Lich",
+                        "monster lvl": 20,
+                        "monster health": 1
+                    }
+                 ]
 const monsterName = document.getElementById("monsterName");
 const monsterLvl = document.getElementById("monsterLvl");
 const monsterHealth = document.getElementById("monsterHealth");
+const statMonster = document.querySelector(".monster");
+let monsterHealthValue = 0;
+let monsterLvlValue = 0;
 
 //Button and Text
 const button1 = document.getElementById("button1");
@@ -57,7 +96,19 @@ const screens = [
         "button text": ["Go Store", "Fight Monster", "Fight Lich"],
         "button function": [goStore, fightMonster, goTown],
         text: "You are in Square Town"
-    }
+    },
+    {
+        "functionName": fightMonster,
+        "button text": ["Attack", "Dodge", "Run"],
+        "button function": [attack, dodge, runFight],
+        text: "You find a monster. The fight start!"
+    },
+    {
+        "functionName": restart,
+        "button text": ["Restart", "Restart", "Restart"],
+        "button function": [restart, restart, restart],
+        text: ""
+    },
 ];
 
 
@@ -173,10 +224,144 @@ function sellAllWeapon(){
     }
 }
 
+// __ -- __ FIGHT FUNCTIONS __ -- __
+
+//
 function fightMonster(){
-    //vazia
+    updateScreen(screens[4]);
+    //FIND MONSTER
+    statMonster.style.display = "grid";
+    if(xp < 100){
+        //find lower lvl monster
+        monsterUpdate(monsters[Math.floor(Math.random()*1)]);
+    } else if(xp > 101 && xp < 350){
+        //find medium lvl monster
+        monsterUpdate(monsters[Math.floor(Math.random()*3)]);
+    } else if(xp > 351 && xp < 800){
+        //find hight lvl monster
+        monsterUpdate(monsters[Math.floor(Math.random()*5)]);
+    } else {
+        //find all monsters
+        monsterUpdate(monsters[Math.floor(Math.random()*6)]);
+    }
 }
 
+function fightLich(){
+    updateScreen(screens[4]);
+    statMonster.style.display = "grid";
+    monsterUpdate(monsters[6]);
+    screenText.innerText = `You are fighting with Lich, the final boss! \n Please save us.`
+}
+
+function attack(){
+    let giveDamage = Math.floor(Math.random()*(xp/2 + 10)) + ((currentWeapon + 1) * 5);
+    if(giveDamage > 200){
+        giveDamage = 150;
+    }
+    let receiveDamage = Math.floor(Math.random() * 21) + monsterLvlValue*2;
+    monsterHealthValue -= giveDamage;
+    monsterHealth.innerText = monsterHealthValue;
+    health -= receiveDamage;
+    healthText.innerText = health;
+    screenText.innerText = `You hit ${giveDamage} on monster. \n`
+    screenText.innerText +=`The monster attack you, -${receiveDamage} health`;
+
+    if(monsterHealthValue <= 0){
+        
+        if(monsterLvlValue == 20){
+            winGame();
+        } else {
+            winFight();
+        }
+    }
+
+    if(health <= 0){
+        lose();
+    }
+
+   
+}
+
+function winFight(){
+    statMonster.style.display = "none";
+    goTown();
+    let rewardXp = Math.floor(Math.random()*monsterLvlValue*30);
+    let rewardGold = Math.floor(Math.random() * (monsterLvlValue * 50));
+    screenText.innerText = "You in the fight and return to Town Square.\n";
+    gold += rewardGold;
+    goldText.innerText = gold;
+    xp += rewardXp;
+    xpText.innerText = xp;
+    screenText.innerText += `You reward fight are +${rewardGold} gold and +${rewardXp} xp.`
+
+}
+
+function winGame(){
+    updateScreen(screens[5]);
+    statMonster.style.display = "none";
+    screenText.innerText += "Thank you! Finally we can living free again! \n"
+    screenText.innerText += "You are the best!"
+
+}
+
+function lose(){
+    confirm("You lose. The game are restarting now.")
+    restart();
+}
+
+function restart(){
+    xp = 0;
+    gold = 50;
+    inventory.splice(0, inventory.length)
+    inventory.push(weapons[0])
+    currentWeapon = 0;
+    health = 150;
+
+    xpText.innerText = xp;
+    goldText.innerText = gold;
+    currentWeaponText.innerText = weapons[currentWeapon];
+    healthText.innerText = health;
+
+    
+}
+
+function monsterUpdate(monster){
+    monsterName.innerText = monster["monster name"];
+    monsterLvl.innerText = monster["monster lvl"];
+    monsterHealth.innerText = monster["monster health"];
+    monsterHealthValue = monster["monster health"];
+    monsterLvlValue = monster["monster lvl"];
+}
+
+function dodge(){
+    if(Math.random() > .12){
+        screenText.innerText = "Monster attack, but you dodge."
+    } else {
+    let receiveDamage = Math.floor(Math.random() * 21) + monsterLvlValue*2;
+    health -= receiveDamage;
+    healthText.innerText = health;
+    screenText.innerText =`The monster attack you, -${receiveDamage} health`;
+    if(health <= 0) {
+        lose();
+    }
+    }
+}
+
+function runFight(){
+    if(Math.random() > .3){
+        statMonster.style.display = "none";
+        goTown();
+        screenText.innerText = "You run the fight and return to Town Square."
+    } else {
+        let receiveDamage = Math.floor(Math.random() * 21) + monsterLvlValue*2;
+        health -= receiveDamage;
+        healthText.innerText = health;
+        screenText.innerText =`You can't run and the monster attack you, -${receiveDamage} health`;
+        if(health <= 0) {
+            lose();
+        }
+        }
+}
 
 
 
